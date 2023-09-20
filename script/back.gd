0b28631ae740bca1f14d85234ec12ceee274333c
27794 <2779435956@qq.com>
2023-09-20 20:34:04 +0800

    [doc]:doc v1

--- script/back.gd
+++ script/back.gd
@@ -1,8 +1,6 @@
 extends Camera2D
 
 
-# Declare member variables here. Examples:
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
