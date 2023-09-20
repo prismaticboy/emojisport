eb73052fc459cd4d50a03ab1e68dd5443344d578
27794 <2779435956@qq.com>
2023-09-20 20:41:25 +0800

    [doc]:doc v1

--- script/back.gd
+++ script/back.gd
@@ -5,7 +5,6 @@ extends Camera2D
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	self.position.x+=1;
